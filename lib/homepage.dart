f98f958c3982d3378bf15e547e019914cb23633c
Tagtiz <61782998+Tagtiz@users.noreply.github.com>
2021-11-03 17:34:55 +0700

    Limits top and bottom, move background
    Limit the height of the player, added background img and moved it

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -17,7 +17,8 @@ class _HomepageState extends State<Homepage> {
   double time = 0;
   double gravity = -4.9;
   // double gravity = -0.1;
-  double velocity = 10;
+  double velocity = 5;
+  double bgX = 0;
 
   bool gamestarted = false;
   void startGame() {
@@ -26,12 +27,20 @@ class _HomepageState extends State<Homepage> {
     Timer.periodic(Duration(milliseconds: 50), (timer) {
       height = gravity * time * time + velocity * time;
       setState(() {
+        bgX -= 5;
+        if (initialPos - height < -1) {
+          initialPos = -1;
+          height = 0;
+        } else if (initialPos - height > 1) {
+          initialPos = 1;
+          height = 0;
+        }
         gamebirdY = initialPos - height;
       });
-      if (gamebirdY < -50 || gamebirdY > 50) {
-        timer.cancel();
-        print("cancelled");
-      }
+      // if (gamebirdY < -5 || gamebirdY > 10) {
+      // timer.cancel();
+      //   print("cancelled");
+      // }
       time += 0.1;
     });
   }
@@ -56,18 +65,37 @@ class _HomepageState extends State<Homepage> {
               Expanded(
                 flex: 15,
                 child: Container(
-                  color: Colors.blue,
+                  // decoration: BoxDecoration(
+                  //     image: DecorationImage(
+                  //         image: AssetImage("lib/assets/background.png"),
+                  //         fit: BoxFit.cover)),
+                  // color: Colors.blue,
                   child: Center(
                     child: Stack(
                       children: [
+                        AnimatedPositioned(
+                          top: 0,
+                          left: bgX,
+                          height: 600,
+                          width: 1050,
+                          child: Image.asset("lib/assets/background.png"),
+                          duration: Duration(microseconds: 200),
+                        ),
+                        AnimatedPositioned(
+                          top: 0,
+                          left: 1050 + bgX,
+                          height: 600,
+                          width: 1050,
+                          child: Image.asset("lib/assets/background.png"),
+                          duration: Duration(microseconds: 200),
+                        ),
                         Container(
-                          alignment: Alignment(-10, -8),
-                          height: 50,
-                          width: 50,
-                          color: Colors.black,
+                          alignment: Alignment(0, gamebirdY),
+                          height: 1050, //50
+                          width: 600, //50
                           child: Container(
-                            width: 10,
-                            height: 10,
+                            width: 100,
+                            height: 100,
                             color: Colors.purple,
                           ),
                         )
