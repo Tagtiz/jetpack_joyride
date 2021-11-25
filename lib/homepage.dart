67a2ecc121773b5f575734bbfe8c3ab49d150466
Tagtiz <61782998+Tagtiz@users.noreply.github.com>
2021-11-25 16:05:45 +0700

    Coin generation and collision detection

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -27,6 +27,8 @@ class _HomepageState extends State<Homepage> {
   int startYCoin = 0;
   int halfYCoin = 0;
   List<double> coinYLoc = [0.0, 0.0, 0.0, 0.0, 0.0];
+  List<double> coinXOffset = [0.0, 0.2, 0.4, 0.6, 0.8];
+  List<bool> coinVis = [true, true, true, true, true];
   void setCoinPath(x) {
     switch (x) {
       case 0:
@@ -54,18 +56,27 @@ class _HomepageState extends State<Homepage> {
           break;
         }
     }
-    randCoin = randomizer.nextInt(3);
+    if (randCoin == 3 || randCoin == 2) {
+      randCoin = randomizer.nextInt(2); // 0 1
+    } else if (randCoin == 0) {
+      randCoin = randomizer.nextInt(2) + 1; // 1 2
+    } else {
+      randCoin = randomizer.nextInt(3); // 0 1 2
+    }
     if (randCoin == 0) {
-      for (int i = 1; i <= 4; i++) {
-        coinYLoc[i] = coinYLoc[i - 1] + 0.15;
-      }
-    } else if (randCoin == 1) {
+      //upper
       for (int i = 1; i <= 4; i++) {
         coinYLoc[i] = coinYLoc[i - 1] - 0.15;
       }
-    } else {
+    } else if (randCoin == 1) {
+      //straight
       for (int i = 1; i <= 4; i++) {
         coinYLoc[i] = coinYLoc[i - 1];
+      }
+    } else {
+      for (int i = 1; i <= 4; i++) {
+        //down
+        coinYLoc[i] = coinYLoc[i - 1] + 0.15;
       }
     }
   }
@@ -82,6 +93,9 @@ class _HomepageState extends State<Homepage> {
         if (bgX <= -1050) {
           bgX = 0;
           setCoinPath(randomizer.nextInt(5));
+          for (var i in coinVis) {
+            i = true;
+          }
         } else {
           bgX -= 5;
         }
@@ -93,6 +107,11 @@ class _HomepageState extends State<Homepage> {
           height = 0;
         }
         gamebirdY = initialPos - height;
+        for (int i = 0; i < 5; i++) {
+          if (collision(i)) {
+            coinVis[i] = false;
+          }
+        }
       });
       // if (gamebirdY < -5 || gamebirdY > 10) {
       // timer.cancel();
@@ -100,6 +119,18 @@ class _HomepageState extends State<Homepage> {
       // }
       time += 0.1;
     });
+  }
+
+  bool collision(int c) {
+    double cx = bgX / 200 + 1.2;
+    double cy = (gamebirdY - coinYLoc[c]).abs();
+    if (sqrt(pow(cx, 2) + pow(cy, 2)) < 0.1) {
+      print('oh no!');
+      return true;
+    } else {
+      print('oh yes');
+      return false;
+    }
   }
 
   void jump() {
@@ -158,60 +189,72 @@ class _HomepageState extends State<Homepage> {
                           duration: Duration(microseconds: 200),
                         ),
                         Container(
-                          alignment: Alignment(0, gamebirdY),
+                          alignment: Alignment(-1, gamebirdY),
                           // height: 1050, //50
                           // width: 600, //50
                           child: Container(
-                            width: 100,
-                            height: 100,
+                            width: 50,
+                            height: 50,
                             child: Image.asset("lib/assets/flying.png"),
                             // color: Colors.purple,
                           ),
                         ),
                         Container(
-                          alignment: Alignment(bgX / 200 + 1.2, coinYLoc[0]),
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.yellow,
+                            alignment: Alignment(
+                                bgX / 200 + 1.2 + coinXOffset[0], coinYLoc[0]),
+                            child: Visibility(
+                              visible: coinVis[0],
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: Colors.yellow,
+                              ),
+                            )),
+                        Container(
+                          alignment: Alignment(
+                              bgX / 200 + 1.2 + coinXOffset[1], coinYLoc[1]),
+                          child: Visibility(
+                            visible: coinVis[1],
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              color: Colors.yellow,
+                            ),
                           ),
                         ),
                         Container(
-                          alignment:
-                              Alignment(bgX / 200 + 1.2 + 0.2, coinYLoc[1]),
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.yellow,
-                          ),
-                        ),
+                            alignment: Alignment(
+                                bgX / 200 + 1.2 + coinXOffset[2], coinYLoc[2]),
+                            child: Visibility(
+                              visible: coinVis[2],
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: Colors.yellow,
+                              ),
+                            )),
                         Container(
-                          alignment:
-                              Alignment(bgX / 200 + 1.2 + 0.4, coinYLoc[2]),
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.yellow,
-                          ),
-                        ),
+                            alignment: Alignment(
+                                bgX / 200 + 1.2 + coinXOffset[3], coinYLoc[3]),
+                            child: Visibility(
+                              visible: coinVis[3],
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: Colors.yellow,
+                              ),
+                            )),
                         Container(
-                          alignment:
-                              Alignment(bgX / 200 + 1.2 + 0.6, coinYLoc[3]),
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.yellow,
-                          ),
-                        ),
-                        Container(
-                          alignment:
-                              Alignment(bgX / 200 + 1.2 + 0.8, coinYLoc[4]),
-                          child: Container(
-                            width: 50,
-                            height: 50,
-                            color: Colors.yellow,
-                          ),
-                        ),
+                            alignment: Alignment(
+                                bgX / 200 + 1.2 + coinXOffset[4], coinYLoc[4]),
+                            child: Visibility(
+                              visible: coinVis[4],
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: Colors.yellow,
+                              ),
+                            )),
                       ],
                     ),
                   ),
